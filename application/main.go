f0b7bdc44df187edd7896a1901d29844a0d1132b
Tony Mu <tonymuwork@gmail.com>
2024-05-23 17:54:36 +0000

    make test app interactive

--- application/main.go
+++ application/main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"glfs/client"
 	"glfs/common"
 	"log"
+	"os"
+	"strings"
 )
 
 // There must be a running GFS cluster
 func main() {
-	testcase := flag.String("t", "", "Which testcase to run?")
+	mode := flag.String("-m", "i", "running mode, i for interactive.")
 	flag.Parse()
 
 	client := client.GLFSClient{}
 	client.Initialize()
 
-	switch *testcase {
-	case "simple_create":
-		client.Create(common.GetTmpPath("client", "test_0.dat"))
-	case "simple_delete":
-		client.Create(common.GetTmpPath("client", "test_0.dat"))
-		client.Delete("test_0.dat")
-	default:
-		log.Printf("Testcase not supported, please check spelling!")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Enter command, create/delete/read etc. or q to quit.")
+		command, _ := reader.ReadString('\n')
+		command = strings.Trim(command, "\n")
+		fmt.Println("cmd: %v, %v", command, command == "q")
+
+		switch command {
+		case "create":
+			client.Create(common.GetTmpPath("client", "test_0.dat"))
+		case "delete":
+			client.Delete("test_0.dat")
+		case "read":
+			client.Read("test_0.dat")
+
+		case "q":
+			os.Exit(0)
+
+		default:
+			log.Printf("Testcase not supported, please check spelling!")
+		}
 	}
+
 }
