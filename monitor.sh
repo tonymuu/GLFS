c9dad21a7b806817542f6007e7686f6514a2ddcf
Tony Mu <tonymuwork@gmail.com>
2024-05-24 03:00:09 +0000

    add master monitor and restart if needed

---
+++ monitor.sh
@@ -0,0 +1,15 @@
+while true; do
+    # find glfs master server acount
+    n=$(ps aux | grep "[g]lfs -role master"| wc -l)
+
+    echo "Found $n master server process..."
+
+    # if cannot find master 
+    if [ "$n" -lt 1 ]; then
+        echo "Restarting master..."
+        ./build/glfs -role master &
+        echo "Master started."
+    fi
+
+    sleep 2
+done
